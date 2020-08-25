0c3c7838cee80a87d8534314d311b92b758d16a1
ulejnar <ulejnar@yahoo.de>
2020-08-25 10:45:31 -0700

    Updates

---
+++ precache-manifest.deefe9da2412b6198ebdd34535ee02d6.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a25843f20d5b52d62974530db0a6e686",
+    "url": "/react-personal-website/index.html"
+  },
+  {
+    "revision": "e1117ff78324cb5cef8b",
+    "url": "/react-personal-website/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "8d69fd826b568d4b3b04",
+    "url": "/react-personal-website/static/css/main.8b22242c.chunk.css"
+  },
+  {
+    "revision": "e1117ff78324cb5cef8b",
+    "url": "/react-personal-website/static/js/2.a201d2a9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-personal-website/static/js/2.a201d2a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d69fd826b568d4b3b04",
+    "url": "/react-personal-website/static/js/main.68267e9c.chunk.js"
+  },
+  {
+    "revision": "449ac624e12ca54c0d36",
+    "url": "/react-personal-website/static/js/runtime-main.0e45652e.js"
+  },
+  {
+    "revision": "0a28bae3078dae8ce3512615399906d1",
+    "url": "/react-personal-website/static/media/Background.0a28bae3.jpeg"
+  },
+  {
+    "revision": "fbf5510c453926e969fb9501226ddc68",
+    "url": "/react-personal-website/static/media/Jeopardy.fbf5510c.png"
+  },
+  {
+    "revision": "0ef300833f29facc1d0be79445b3e659",
+    "url": "/react-personal-website/static/media/Jobly.0ef30083.png"
+  },
+  {
+    "revision": "9ea2a6b0501ec2f910b19da1d5925801",
+    "url": "/react-personal-website/static/media/Memory_game.9ea2a6b0.png"
+  },
+  {
+    "revision": "ae83e65da2edc7f1790af7bc2341e00e",
+    "url": "/react-personal-website/static/media/Ulrika-edited.ae83e65d.jpg"
+  },
+  {
+    "revision": "9bea76e57f555bd5ea880ec1266107c3",
+    "url": "/react-personal-website/static/media/Warbler.9bea76e5.png"
+  },
+  {
+    "revision": "bae3f25b97bf83d2a461de516e85761e",
+    "url": "/react-personal-website/static/media/Website.bae3f25b.png"
+  }
+]);
